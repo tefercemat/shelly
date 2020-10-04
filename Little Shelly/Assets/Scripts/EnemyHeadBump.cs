12a56bc0d5c7c46f586e4139549363423bbfdfea
tefercemat <tefercemat@hotmail.com>
2020-10-04 18:34:32 +1100

    Setup Crab Enemy

---
+++ Little Shelly/Assets/Scripts/EnemyHeadBump.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyHeadBump : MonoBehaviour
+{
+    // if Player hits the stun point of the enemy, then call Stunned on the enemy
+	void OnCollisionEnter2D(Collision2D other)
+	{
+		if (other.gameObject.tag == "Player")
+		{
+			// tell the enemy to be stunned
+			this.GetComponentInParent<CrabEnemy>().Stunned();
+
+			// Make the player bounce off the enemy
+			//other.gameObject.GetComponent<PlayerController>().EnemyBounce();
+		}
+	}
+}
